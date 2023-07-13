8b4e5b86d039427d24a489d773408e221dd69b9c
An041003 <binhan214981@gmail.com>
2023-07-13 20:17:08 +0700

    FE

---
+++ client/Front-End/Trang chủ/script.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', function() {
+    // Lấy DATA
+    const userData = {
+      subject: 'XXX'
+    };
+  
+    // Nạp DATA
+    document.getElementsByClass('subject').value = userData.subject;
+  });
+  
